71a475b250767e5557317ab06d8b1805b6a96ca6
Bhupendra <sainibhupendra50042@gmail.com>
2024-02-15 14:41:49 +0530

    initial commit

---
+++ Project 2 js2.js
@@ -0,0 +1,101 @@
+console.log('this is 2js version of project 2');
+class book{
+ constructor(name, author, type) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
+}
+
+class display{
+    add(book) {
+        console.log("Adding to UI");
+        let tableBody = document.getElementById('tableBody');
+        let uiString = `
+                       <tr>
+                             <td>${book.name}</td>
+                             <td>${book.author}</td>
+                             <td>${book.type}</td>
+                       </tr>`;
+        tableBody.innerHTML += uiString;
+    }
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        libraryForm.reset();
+    }
+    validate(book) {
+        if (book.name.length < 2 || book.author.length < 2) {
+            return false
+        }
+        else {
+            return true;
+        }
+    }
+    show(type, displayMessage) {
+        let message = document.getElementById('message');
+       let boldText;
+        if(type==='success'){
+            boldText = 'success';
+        }
+        else{
+            boldText='Error!';
+        }
+        message.innerHTML = ` <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>${boldText}:</strong>  ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>`;
+    
+        setTimeout(function () {
+            message.innerHTML = ''
+        }, 5000);
+    
+    }
+
+    
+}
+//Add submit event listener libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('You have submitted library form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+
+    let type;
+    //fiction programming, cooking
+
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'your book has been successfully added')
+    }
+    else {
+        //show error to the user
+        display.show('danger', 'sorry you cannot add this book')
+    }
+
+    e.preventDefault();
+
+}
